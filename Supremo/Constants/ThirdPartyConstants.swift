b4be3927cf7a17bc606a599cc8cc576db8210afc
Sharad Chauhan <sharad@getinfino.com>
2020-11-14 10:22:45 +0530

    feat: aded third party constants

---
+++ Supremo/Constants/ThirdPartyConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ThirdPartyConstants.swift
+//  Supremo
+//
+//  Created by Sharad on 14/11/20.
+//
+
+import Foundation
+
+struct SuperHeroAPIConstants {
+    static let accessToken = "3392554877466116"
+}
