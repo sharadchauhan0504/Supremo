8c415c89e214ead684562f837608f420e0da12f6
Sharad Chauhan <sharad@getinfino.com>
2020-11-14 10:25:47 +0530

    tests: added protocols for urlsession data task

---
+++ Supremo/Network/API Mock Classes/URLSessionDataTaskProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  URLSessionDataTaskProtocol.swift
+//  Supremo
+//
+//  Created by Sharad on 14/11/20.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+    typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol
+}
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping URLSessionProtocol.DataTaskResult) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
