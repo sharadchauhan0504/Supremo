2121ac8cec4dd258e2cabf5acd0029be64e6b8a7
Sharad Chauhan <sharad@getinfino.com>
2020-11-14 10:26:43 +0530

    feat: added http methods

---
+++ Supremo/Network/API Structure/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Supremo
+//
+//  Created by Sharad on 14/11/20.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
