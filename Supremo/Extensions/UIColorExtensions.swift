984043b25b4c6a1499ca2db3fd46263d294639aa
Sharad Chauhan <sharad@getinfino.com>
2020-11-14 10:23:45 +0530

    feat: added warm pink colour

---
+++ Supremo/Extensions/UIColorExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtensions.swift
+//  Supremo
+//
+//  Created by Sharad on 14/11/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var warmPink: UIColor {
+        return UIColor(red: 247.0 / 255.0, green: 82.0 / 255.0, blue: 126.0 / 255.0, alpha: 1.0)
+    }
+    
+}
