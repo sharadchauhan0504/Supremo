f92ca4612bc262be36f7853d39ae79164706e08f
Sharad Chauhan <sharad@getinfino.com>
2020-11-15 20:50:45 +0530

    feat:  added CharacterDetailTableData - added table cases

---
+++ Supremo/Modules/Character Detail Screen/Data/CharacterDetailTableData.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterDetailTableData.swift
+//  Supremo
+//
+//  Created by Sharad on 15/11/20.
+//
+
+import Foundation
+
+enum CharacterDetailTableData: CaseIterable {
+    
+    case basic
+    case appearance
+    case powerstats
+    case biography
+    
+    
+    
+}
