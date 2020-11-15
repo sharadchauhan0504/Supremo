59b19b8ac600a7d43fe6d3f8cb4bf1b1f841b0b5
Sharad Chauhan <sharad@getinfino.com>
2020-11-15 20:56:20 +0530

    feat: added SearchSuperHeroesErrorOutput for error response

---
+++ Supremo/Network/Codables/Outputs/SearchSuperHeroesErrorOutput.swift
@@ -0,0 +1,13 @@
+//
+//  SearchSuperHeroesErrorOutput.swift
+//  Supremo
+//
+//  Created by Sharad on 15/11/20.
+//
+
+import Foundation
+
+// MARK: - SearchSuperHeroesErrorOutput
+struct SearchSuperHeroesErrorOutput: Codable {
+    let response, error: String
+}
