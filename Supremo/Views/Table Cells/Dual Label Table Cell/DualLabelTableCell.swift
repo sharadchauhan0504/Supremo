553067a2e0f150adf45a85ffb53f68809b1e5c9c
Sharad Chauhan <sharad@getinfino.com>
2020-11-15 20:59:03 +0530

    feat: added DualLabelTableCell

---
+++ Supremo/Views/Table Cells/Dual Label Table Cell/DualLabelTableCell.swift
@@ -0,0 +1,22 @@
+//
+//  DualLabelTableCell.swift
+//  Supremo
+//
+//  Created by Sharad on 15/11/20.
+//
+
+import UIKit
+
+class DualLabelTableCell: UITableViewCell {
+
+    //MARK:- IBOutlets
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    
+    //MARK:- Lifecycle methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
