8b03a8ba35942bf00059f1607bde464a8d28aa34
Sharad Chauhan <sharad@getinfino.com>
2020-11-16 14:08:04 +0530

    feat: added SingleLabelTableCell

---
+++ Supremo/Views/Table Cells/Single Label Table Cell/SingleLabelTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  SingleLabelTableCell.swift
+//  Supremo
+//
+//  Created by Sharad on 16/11/20.
+//
+
+import UIKit
+
+class SingleLabelTableCell: UITableViewCell {
+
+    //MARK:- IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    //MARK:- Lifecycle methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
