7b8896bfd468910e6f3a11ea6c113945a7a3e751
Sharad Chauhan <sharad@getinfino.com>
2020-11-16 12:45:22 +0530

    feat: added table data properties

--- Supremo/Modules/Character Detail Screen/Data/CharacterDetailTableData.swift
+++ Supremo/Modules/Character Detail Screen/Data/CharacterDetailTableData.swift
@@ -5,7 +5,7 @@
 //  Created by Sharad on 15/11/20.
 //
 
-import Foundation
+import UIKit
 
 enum CharacterDetailTableData: CaseIterable {
     
@@ -13,7 +13,44 @@ enum CharacterDetailTableData: CaseIterable {
     case appearance
     case powerstats
     case biography
+        
+    var identifier: String {
+        switch self {
+        case .basic: return String(describing: BasicDetailsTableCell.self)
+        case .appearance, .biography: return String(describing: DualLabelTableCell.self)
+        case .powerstats: return String(describing: PowerStatsTableCell.self)
+        }
+    }
     
+    var rowHeight: CGFloat {
+        switch self {
+        case .basic: return 300
+        case .appearance, .biography, .powerstats: return UITableView.automaticDimension
+        }
+    }
     
+    var headerHeight: CGFloat {
+        switch self {
+        case .basic: return 0
+        case .appearance, .biography, .powerstats: return 60
+        }
+    }
     
+    var headerTitle: String {
+        switch self {
+        case .basic: return "BASIC"
+        case .appearance: return "APPEARANCE"
+        case .biography: return "BIOGRAPHY"
+        case .powerstats: return "POWER STATS"
+        }
+    }
+    
+    var numberOfRows: Int {
+        switch self {
+        case .basic: return 1
+        case .appearance: return 4
+        case .biography: return 4
+        case .powerstats: return 6
+        }
+    }
 }
